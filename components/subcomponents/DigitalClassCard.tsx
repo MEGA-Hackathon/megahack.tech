e1128239b03602cf68c1d1bff574e90be7323962
Mootbing <50122069+Mootbing@users.noreply.github.com>
2023-03-30 14:03:31 -0400

    works

--- components/subcomponents/DigitalClassCard.tsx
+++ components/subcomponents/DigitalClassCard.tsx
@@ -18,7 +18,7 @@ const DigitalClassCard = ({
                 borderStyle: "solid",
                 borderColor: "#000",
                 borderRadius: 25,
-                backgroundImage: "url('https://i.pinimg.com/originals/a3/94/84/a394841d08da55f893616b74e07f00a5.jpg')",
+                backgroundImage: videoLink == "N/A" ? "url('https://i.pinimg.com/originals/a3/94/84/a394841d08da55f893616b74e07f00a5.jpg')" : "url('https://media.istockphoto.com/vectors/loading-icon-isolated-with-long-shadow-progress-bar-icon-flat-design-vector-id1081607416?k=6&m=1081607416&s=170667a&w=0&h=N2EZ8-BirboWf1zh3SRWL4pfiU4cRJh1_u0vM8AN_Ck=')",
             }}>
                 {videoLink != "N/A" && <iframe style={{borderRadius: 25, height: "100%", width: "100%"
                 }} src={"https://www.youtube.com/embed/" + videoLink} title="Player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />}
@@ -27,8 +27,11 @@ const DigitalClassCard = ({
                     fontFamily: "Poppins",
                     color: "#fff",
                     backgroundColor: "rgba(0,0,0,0.5)",
-                    backdropFilter: "blur(10px)",
-                    padding: 25,
+                    backdropFilter: "blur(1px)",
+                    width: "100%",
+                    height: "100%",
+                    textAlign: "center",
+                    display: "flex",
                 }}>No video for {name}</p>}
             </div>
             <div className="event-card-content">
