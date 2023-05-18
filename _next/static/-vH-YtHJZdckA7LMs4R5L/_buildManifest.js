7565b0f64b9f696662449bef394386c12d72bf1a
Mootbing <50122069+Mootbing@users.noreply.github.com>
2023-05-18 09:51:26 -0400

    rid

---
+++ _next/static/-vH-YtHJZdckA7LMs4R5L/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/664-e2359597ac708d8d.js","static/chunks/pages/index-d444415d534fd18a.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
